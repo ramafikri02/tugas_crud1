ea7bfb809338eee396576ed7f874d88dbc67f98a
Muhammad Ramadhan Fikri <55729945+ramafikri02@users.noreply.github.com>
2020-04-13 18:02:38 +0700

    update proses_simpan_makanan.php
    Penambahan id otomatis di PHP

--- proses_simpan_makanan.php
+++ proses_simpan_makanan.php
@@ -7,9 +7,13 @@ $satuan = $_POST{'satuan'};
 $kategori = $_POST{'kategori'};
 $gambar = $_POST{'gambar'};
 $stok = $_POST{'stok'};
-// print_r($_POST);
+
+$cari_id = mysqli_query($kon, "SELECT * from makanan order by id_makanan DESC LIMIT 0,1");
+$i = mysqli_fetch_array($cari_id);
+$id_awal = (int) ($i['id_makanan'])+1;
+
 $perintah = mysqli_query($kon, "INSERT INTO `makanan` (`id_makanan`, `nama_makanan`, `harga`, `satuan`, `kategori`, `url_gambar`, `stok`) 
-VALUES (NULL, '$nama', '$harga', '$satuan', '$kategori', '$gambar', '$stok');");
+VALUES ('$id_awal', '$nama', '$harga', '$satuan', '$kategori', '$gambar', '$stok');");
 
 if ($perintah==TRUE) {
     header("Location:data_makanan.php");
